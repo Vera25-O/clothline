7c337d6c9da18a1770d7ed65b4803a480eb209a3
Vera Onuko <vera@DESKTOP-QPRLS4I.localdomain>
2023-02-17 10:32:52 +0300

    created files

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import Home from "./Components/Home";
+function App() {
+  return (
+    <div className="App">
+     <Home/> 
+          
+        
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/Components/Home.js
@@ -0,0 +1,14 @@
+
+function Home() {
+    return (
+      <div className="home">
+        <h1>HOLLA</h1>
+            
+          
+        
+      </div>
+    );
+  }
+  
+  export default Home;
+  
