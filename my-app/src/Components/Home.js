0fc5e51609d955c8244cd49f40e02062edae2515
Vera Onuko <vera@DESKTOP-QPRLS4I.localdomain>
2023-02-17 10:49:36 +0300

    added homepage photos

--- my-app/src/Components/Home.js
+++ my-app/src/Components/Home.js
@@ -1,15 +1,16 @@
-import line1 from "./photos/line 1.jpeg"
+import line1 from "./photos/line1.jpeg";
 
 function Home() {
-    return (
-      <div className="home">
-        
-            
-          
-        
-      </div>
-    );
-  }
-  
-  export default Home;
-  
+  return <div className="home">
+    <img src={line1} alt="" className="homp"/>
+    <img src={line1} alt="" className="homp"/>
+    <img src={line1} alt="" className="homp"/>
+    <img src={line1} alt="" className="homp"/>
+    <img src={line1} alt="" className="homp"/>
+    <img src={line1} alt="" className="homp"/>
+    <img src={line1} alt="" className="homp"/>
+    <img src={line1} alt="" className="homp"/>
+  </div>;
+}
+
+export default Home;
