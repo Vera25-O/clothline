bc404ecbf6d75ad611975e7d9117a4ec3117a17b
Vera Onuko <vera@DESKTOP-QPRLS4I.localdomain>
2023-02-17 13:02:54 +0300

    styled home images

--- my-app/src/Components/Home.js
+++ my-app/src/Components/Home.js
@@ -1,19 +1,20 @@
-import line1 from "./photos/line1.jpeg";
-import line2 from "./photos/line2.jpeg";
-import line3 from "./photos/line3.jpeg";
-import line4 from "./photos/line4.jpeg";
-import line5 from "./photos/line5.jpeg";
-import line6 from "./photos/line6.jpeg";
+
+import line3 from "./photos/line2.jpeg";
+import line4 from "./photos/line3.jpeg";
+import line5 from "./photos/line4.jpeg";
+import line1 from "./photos/line5.jpeg";
+import line2 from "./photos/line6.jpeg";
 
 function Home() {
   return <div className="home">
-    <h1>HOLLA</h1>
+    <h1>GALLERY</h1>
     <img src={line1} alt="" className="homp1"/>
     <img src={line2} alt="" className="homp2"/>
     <img src={line3} alt="" className="homp3"/>
     <img src={line4} alt="" className="homp4"/>
-    <img src={line5} alt="" className="homp"/>
-    <img src={line6} alt="" className="homp5"/>
+    <img src={line5} alt="" className="homp5"/>
+    
+    
     {/* <img src={line1} alt="" className="homp"/>
     <img src={line1} alt="" className="homp"/> */}
   </div>;
