d54977ffaaf82086e06374e8f9bd7cef5f1f7a13
Vera Onuko <vera@DESKTOP-QPRLS4I.localdomain>
2023-02-24 16:43:53 +0300

    modified css

--- my-app/src/Components/Blog.js
+++ my-app/src/Components/Blog.js
@@ -1,7 +1,11 @@
 function Blog(){
     return(
         <div>
-
+<div class="flex-container">
+  <div>1</div>
+  <div>2</div>
+  <div>3</div>  
+</div>
         </div>
     );
 }
--- my-app/src/Components/Navone.js
+++ my-app/src/Components/Navone.js
@@ -2,7 +2,7 @@ function Navone()
 {
     return(
 <div className="navone">
-
+<image src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSU8Iisdd7RFZYlklKuZS1ggYQPybAIEGOrzg&usqp=CAU" alt="icon"/>
 </div>
     )
 }
