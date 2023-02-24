6c122e16c2e99384c10f556f9f1f3fa493c763fb
Vera Onuko <vera@DESKTOP-QPRLS4I.localdomain>
2023-02-24 15:00:50 +0300

    modified index.css

--- my-app/src/Components/Fashion.js
+++ my-app/src/Components/Fashion.js
@@ -9,6 +9,14 @@ function Fashion() {
   <div>2</div>
   <div>3</div>
   <div>4</div>
+
+</div>
+
+<div class="flex-container">
+  <div>1</div>
+  <div>2</div>
+  <div>3</div>
+  <div>4</div>
   
 </div>
 </div>
