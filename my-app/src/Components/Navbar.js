3a2580cc4ef0b2b08eed8679e4694bcc10543521
Vera Onuko <vera@DESKTOP-QPRLS4I.localdomain>
2023-02-24 09:50:28 +0300

    created files

--- my-app/src/Components/Navbar.js
+++ my-app/src/Components/Navbar.js
@@ -3,38 +3,31 @@ import React from "react";
 import { useEffect, useState } from "react";
 
 function Navbar() {
-    const [navbar, setNavbar] = useState(false) 
-   
-   //navbar scroll changeBackground function
-   const changeBackground = () => {
-     console.log(window.scrollY)
-     if (window.scrollY >= 66) {
-       setNavbar(true)
-     } else {
-       setNavbar(false)
-     }
+  const [navbar, setNavbar] = useState(false);
+
+  const changeBackground = () => {
+    console.log(window.scrollY);
+    if (window.scrollY >= 66) {
+      setNavbar(true);
+    } else {
+      setNavbar(false);
     }
+  };
 
-    
+  useEffect(() => {
+    changeBackground();
+    window.addEventListener("scroll", changeBackground);
+  });
 
-    useEffect(() => {
-      changeBackground()
-      // adding the event when scroll change background
-      window.addEventListener("scroll", changeBackground)
-    })
-    
   return (
-    
-    
-        <nav className={navbar ? "navbar active" : "navbar"}>
-        <NavLink to="/" exact="true">
+    <nav className={navbar ? "navbar active" : "navbar"}>
+      <NavLink to="/" exact="true">
         <h7>Home</h7>
       </NavLink>
       <NavLink to="/blogs" exact="true">
         <h7>Blogs</h7>
       </NavLink>
-      </nav>
-    
+    </nav>
   );
 }
 export default Navbar;
