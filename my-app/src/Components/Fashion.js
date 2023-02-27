7537fecfc0d5972708cd50c15f01db28f81a2cd1
Vera Onuko <vera@DESKTOP-QPRLS4I.localdomain>
2023-02-27 12:11:40 +0300

    modified index.css

--- my-app/src/Components/Fashion.js
+++ my-app/src/Components/Fashion.js
@@ -6,20 +6,19 @@ function Fashion() {
         <div>1</div>
         <div>2</div>
         <div>3</div>
-        <div>4</div>
       </div>
 
       <div className="container">
         <div>1</div>
         <div>2</div>
         <div>3</div>
-        <div>4</div>
+        
       </div>
       <div className="container">
         <div>1</div>
         <div>2</div>
         <div>3</div>
-        <div>4</div>
+        
       </div>
     </div>
   );
