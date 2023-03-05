350f081d76dfbc5fc8133dd34a0b00d8718ec34d
Vera Onuko <vera@DESKTOP-QPRLS4I.localdomain>
2023-03-05 19:02:35 +0300

    modified Footer.js

--- my-app/src/Components/Footer.js
+++ my-app/src/Components/Footer.js
@@ -3,7 +3,7 @@
 function Footer() {
   return (
       <div className="footer">
-      <p className="cright">© 2022 veraonuko. All rights reserved.</p>
+      <p className="cright">© 2023 veraonuko. All rights reserved.</p>
     </div>
   );
 }
