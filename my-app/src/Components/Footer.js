215d0f16a5598cbb89a561cca113a64c5846eeec
Vera Onuko <vera@DESKTOP-QPRLS4I.localdomain>
2023-03-08 11:45:46 +0300

    photos added to footer

--- my-app/src/Components/Footer.js
+++ my-app/src/Components/Footer.js
@@ -1,9 +1,13 @@
-
+import icon1 from "./photos/icon1.png";
+import icon2 from "./photos/icon2.png";
 
 function Footer() {
   return (
       <div className="footer">
         <hr></hr>
+        <img src={icon1} alt="facebook" className="icons" />
+<img src={icon2} alt="instagram" className="icons" />
+    
       <p className="cright">© 2023 veraonuko. All rights reserved.</p>
     </div>
   );
