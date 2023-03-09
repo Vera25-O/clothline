add42e5e1060d3ce1ad70b087dca8daf2b60a0fd
Vera Onuko <vera@DESKTOP-QPRLS4I.localdomain>
2023-03-09 14:18:00 +0300

    added anchor tags

--- my-app/src/Components/Navone.js
+++ my-app/src/Components/Navone.js
@@ -4,8 +4,8 @@ function Navone()
 {
     return(
 <div className="navone">
-<img src={icon1} alt="facebook" className="icon1" />
-<img src={icon2} alt="instagram" className="icon2" />
+<a href="https://www.facebook.com/home.php" target={"_blank"}><img src={icon1} alt="facebook" className="icon1" /></a>
+<a href="https://www.instagram.com/?utm_source=pwa_homescreen" target={"_blank"}><img src={icon2} alt="instagram" className="icon2" /></a>
     
     
 </div>
