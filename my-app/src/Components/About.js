a604211588c69d2bf4a9b8d5a30008465cdfa95a
Vera Onuko <vera@DESKTOP-QPRLS4I.localdomain>
2023-03-10 13:31:13 +0300

    modified About.js

--- my-app/src/Components/About.js
+++ my-app/src/Components/About.js
@@ -1,8 +1,25 @@
-function About(){
-    return(
-        <div>
-            
-        </div>
-    )
+function About() {
+  return (
+    <div>
+      <h1>ABOUT US</h1>
+      <p className="abtp">
+        Our fashion Hub is a passion driven thrift business<br></br>
+        It started off when a lady went thrifting her own pants for the
+        semester, she bought like five pieces at a fair prices<br></br>
+        Some pants were not fitting her but she bought them anyway because they
+        had a great fabric and the design was super<br></br>
+        Since she got really nice pants and did not want to leave them, she took
+        them then sold the three pieces to her friends at a fair price<br></br>
+        This very thing brought up the idea of ThriFThUB.Co<br></br>
+      </p>
+      <p className="abtp">
+        ThriFThUB focuses on latest trends and styles. We also give close
+        attention to the fabrics and comfort that comes with it.<br></br>
+        Customer experience and satisfaction is our greatest priority<br></br>
+        We advice on fashion sense and help you the clothe that best fits you
+        <br></br>
+      </p>
+    </div>
+  );
 }
 export default About;
