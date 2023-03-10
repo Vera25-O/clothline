927e364cce5b12271a234e27501345e007d19ec0
Vera Onuko <vera@DESKTOP-QPRLS4I.localdomain>
2023-03-10 15:14:40 +0300

    modified Terms.js

--- my-app/src/Components/Refund.js
+++ my-app/src/Components/Refund.js
@@ -1,7 +1,16 @@
 function Refund(){
     return(
         <div>
-            
+            <h1>REFUND POLICY</h1>
+            <p className="abtp">
+                <p className="underline">Exchanges(If Applicable)</p>
+            We only replace items if they are defective or damaged.<br></br> 
+            If you need to exchange it for the same item, send us an contact us at +254748438571<br></br>
+                <p className="underline">Delivery</p>
+            You will be responsible for paying for your own delivery costs for returning your item.<br></br> 
+            Delivery  costs are non-refundable. If you receive a refund, the cost of return shipping will be deducted from your refund.<br></br>
+            Depending on where you live, the time it may take for your exchanged product to reach you, may vary.<br></br>
+            </p>
         </div>
     )
 }
