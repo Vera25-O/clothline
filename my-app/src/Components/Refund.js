2da232ed1e9badc37de6118c4dd6b523acdd67d7
Vera Onuko <vera@DESKTOP-QPRLS4I.localdomain>
2023-04-29 22:26:46 +0300

    installed dependencies

--- my-app/src/Components/Refund.js
+++ my-app/src/Components/Refund.js
@@ -15,3 +15,43 @@ function Refund(){
     )
 }
 export default Refund;
+
+// import { NavLink } from "react-router-dom";
+// import React from "react";
+// import { useEffect, useState } from "react";
+
+// function Navbar() {
+//   const [navbar, setNavbar] = useState(false);
+
+//   const changeBackground = () => {
+//     console.log(window.scrollY);
+//     if (window.scrollY >= 200) {
+//       setNavbar(true);
+//     } else {
+//       setNavbar(false);
+//     }
+//   };
+
+//   useEffect(() => {
+//     changeBackground();
+//     window.addEventListener("scroll", changeBackground);
+//   });
+
+//   return (
+//     <nav className={navbar ? "navbar active" : "navbar"}>
+      
+//         <h7 className="logo">Nostr</h7>
+//         <h7>Product</h7>
+//         <h7>Solutions</h7>
+//         <h7>Resources</h7>
+//         <h7>|</h7>
+//         <h7>Gallery</h7>
+//         <h7>Pricing</h7>
+//         <h7>Signing</h7>
+//         <button>Request Demo</button>
+//         <button>Get Started</button>
+//     </nav>
+    
+//   );
+// }
+// export default Navbar;
---
+++ my-app/src/Components/SearchBar.js
@@ -0,0 +1,30 @@
+/* <div className="hover-rotate">
+  <img src="https://picsum.photos/id/669/600/800.jpg"/>
+  <img src="https://picsum.photos/id/669/600/800.jpg" className='rotate'/>
+</div>
+
+//css
+.rotate{
+    overflow-x: visible;
+    position: relative;
+  }
+  
+  .hover-rotate {
+    overflow: hidden;
+    margin: 8px;
+    min-width: 240px;
+    max-width: 320px;
+    width: 100%;
+    float: right;
+  }
+  
+  .hover-rotate img {
+    transition: all 0.3s;
+    box-sizing: border-box;
+    max-width: 100%;
+  }
+  
+  .hover-rotate:hover {
+    transform: scale(1.3) rotate(5deg);
+  }
+   */
